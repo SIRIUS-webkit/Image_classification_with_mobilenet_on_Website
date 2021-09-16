766f5cf6e0a44e54624131875eaad3c19107dd9a
SIRIUS-webkit <rogermbl138998@gmail.com>
2021-09-16 10:31:43 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './css/imageclassify.css';
+import './css/responsive.css';
+import Imageclassify from './components/Imageclassify';
+
+function App() {
+  return (
+    <div className="App">
+      <Imageclassify />
+    </div>
+  );
+}
+
+export default App;
